2411ff0d2aebc7ada08396b97dc719b50c42c7dd
Lorraineboza Wilelkwile <pauldanilchenko2000@gmail.com>
2025-05-27 22:20:07 +0300

    Create screen_utils.c

---
+++ src/utils/screen_utils.c
@@ -0,0 +1,14 @@
+#include "screen_utils.h"
+#include "../game/game.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+void screen_init(void) {
+    // Инициализация экрана
+}
+
+void screen_clear(void) {
+    system("cls");
+}
+
+// Остальные функции для работы с экраном
