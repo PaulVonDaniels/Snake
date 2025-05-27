f19c7458425b7c93b5eb108fb72412c1add66caa
Lorraineboza Wilelkwile <pauldanilchenko2000@gmail.com>
2025-05-27 22:19:41 +0300

    Create apple_tree.c

---
+++ src/entities/apple_tree.c
@@ -0,0 +1,15 @@
+#include "apple_tree.h"
+#include "../game/game.h"
+
+AppleTree trees[5];
+int num_trees = 0;
+
+void apple_trees_init(void) {
+    num_trees = 0;
+}
+
+void apple_tree_generate(void) {
+    // Реализация generateAppleTree()
+}
+
+// Остальные функции, связанные с яблонями
